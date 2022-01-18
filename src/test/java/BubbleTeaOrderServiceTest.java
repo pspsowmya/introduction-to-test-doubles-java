9462c8ddaa7a005a47923b1171bd8ba0a04d7903
panthangiswapna <pspsowmya@gmail.com>
2022-01-18 19:33:14 +0000

    Code reformatting changes

--- src/test/java/BubbleTeaOrderServiceTest.java
+++ src/test/java/BubbleTeaOrderServiceTest.java
@@ -60,7 +60,6 @@ public class BubbleTeaOrderServiceTest {
     }
 
 
-
     @ParameterizedTest
     @EnumSource(BubbleTeaTypeEnum.class)
     public void shouldCreateBubbleTeaOrderRequestWhenCreateOrderRequestIsCalledByParameterization(BubbleTeaTypeEnum teaTypeEnum) {
